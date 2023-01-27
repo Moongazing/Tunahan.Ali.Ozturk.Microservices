6edea1d64695e398acd4224061855751f5b3afd0
Tunahan Ali Ozturk <72943708+Moongazing@users.noreply.github.com>
2023-01-28 02:13:16 +0300

    Adding Http Method Names for Swagger

--- Services/Catalog/FreeCourse.Services.Catalog/Controllers/CategoriesController.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Controllers/CategoriesController.cs
@@ -16,7 +16,7 @@ namespace FreeCourse.Services.Catalog.Controllers
     {
       _categoryService = categoryService;
     }
-
+    [HttpGet]
     public async Task<IActionResult> GetAll()
     {
       var categories = await _categoryService.GetAllAsync();
@@ -29,7 +29,7 @@ namespace FreeCourse.Services.Catalog.Controllers
       var category = await _categoryService.GetByIdAsync(id);
       return CreateActionResultInstance(category);
     }
-
+    [HttpPost]
     public async Task<IActionResult> Create(CategoryDto categoryDto)
     {
       var response = await _categoryService.CreateAsync(categoryDto);
