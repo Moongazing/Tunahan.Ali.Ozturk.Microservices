b0d81ec4bd5c1a7effbb8ab27496ae3392f3dced
Tunahan Ali Ozturk <72943708+Moongazing@users.noreply.github.com>
2023-01-30 02:42:23 +0300

    Identity server and some refactoring.

---
+++ IdentityServer/TAO.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using FreeCourse.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using TAO.IdentityServer.Dtos;
+using TAO.IdentityServer.Models;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace TAO.IdentityServer.Controllers
+{
+  
+  [Authorize(LocalApi.PolicyName)]
+  [Route("api/[controller]/{action}")]
+  [ApiController]
+  public class UserController : ControllerBase
+  {
+    private readonly UserManager<ApplicationUser> _userManager;
+    public UserController(UserManager<ApplicationUser> userManager)
+    {
+      _userManager = userManager;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SignUp(SignupDto singupDto)
+    {
+      var user = new ApplicationUser
+      {
+        UserName = singupDto.UserName,
+        Email = singupDto.Email,
+        City = singupDto.City,
+    
+      };
+      var result = await _userManager.CreateAsync(user, singupDto.Password);
+
+      if(!result.Succeeded)
+      {
+        return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x=>x.Description).ToList(),400));
+      }
+      return NoContent();
+    }
+  }
+}
--- Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Startup.cs
@@ -1,5 +1,6 @@
 using FreeCourse.Services.Catalog.Services;
 using FreeCourse.Services.Catalog.Settings;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -28,6 +29,13 @@ namespace FreeCourse.Services.Catalog
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+      {
+        options.Authority = Configuration["IdentityServerURL"];
+        options.Audience = "resource_catalog";
+        options.RequireHttpsMetadata = false;
+
+      });
       services.AddScoped<ICategoryService, CategoryService>();
       services.AddScoped<ICourseService,CourseService>();
 
@@ -46,6 +54,7 @@ namespace FreeCourse.Services.Catalog
       {
         c.SwaggerDoc("v1", new OpenApiInfo { Title = "FreeCourse.Services.Catalog", Version = "v1" });
       });
+   
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -60,6 +69,7 @@ namespace FreeCourse.Services.Catalog
 
       app.UseRouting();
 
+      app.UseAuthentication();
       app.UseAuthorization();
 
       app.UseEndpoints(endpoints =>
