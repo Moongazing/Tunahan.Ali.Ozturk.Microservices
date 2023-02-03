ca1b968265e123fc0cc615eb1ce7bf5e84008317
Tunahan Ali Ozturk <72943708+Moongazing@users.noreply.github.com>
2023-02-03 03:50:24 +0300

    Refactor Identity Server

--- IdentityServer/TAO.IdentityServer/Controllers/UserController.cs
+++ IdentityServer/TAO.IdentityServer/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Threading.Tasks;
 using TAO.IdentityServer.Dtos;
@@ -40,6 +41,20 @@ namespace TAO.IdentityServer.Controllers
         return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x=>x.Description).ToList(),400));
       }
       return NoContent();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUser()
+    {
+      var userIdClaim = User.Claims.FirstOrDefault(x=>x.Type == JwtRegisteredClaimNames.Sub);
+      var user = await _userManager.FindByIdAsync(userIdClaim.Value);
+
+      if (userIdClaim == null || user == null ) 
+      {
+        return BadRequest();
+      }
+      return Ok(new {Id = user.Id, UserName = user.UserName, Email= user.Email,City = user.City});
+      
     }
   }
 }
